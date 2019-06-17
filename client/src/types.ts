dde8d9cf363f1023fe4d35be11aad8b5c5c2cbf4
Mohammed Shahoe <muyabb@yahoo.com>
2019-06-17 10:34:17 +0300

    removed materials ui and added semantic ui react

--- client/src/types.ts
+++ client/src/types.ts
@@ -2,4 +2,19 @@ export interface Transaction {
 	description: string;
 	amount: number;
 	date: Date;
+	type: TransactionType;
+	category?: string;
+	tag?: string;
+}
+
+export interface Item {
+	name: string;
+	quantity: number;
+	transaction: Transaction;
+	price: number;
+}
+
+export enum TransactionType {
+	Buy,
+	Sell
 }
